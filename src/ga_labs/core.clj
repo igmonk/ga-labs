7dd82a8d2dfb9d097927c14e229497c02c816284
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-11-25 19:02:07 +0100

    Initial commit.

---
+++ src/ga_labs/core.clj
@@ -0,0 +1,7 @@
+(ns ga-labs.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/ga_labs/core_test.clj
@@ -0,0 +1,7 @@
+(ns ga-labs.core-test
+  (:require [clojure.test :refer :all]
+            [ga-labs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
